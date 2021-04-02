a7119b4a2ef3e4caff7082a4d9986f1801684354
SamDeBlock <6572609+SamDeBlock@users.noreply.github.com>
2021-04-02 12:52:57 +0200

    Reconnect on all kinds of errors

--- src/main/java/io/github/sac/Socket.java
+++ src/main/java/io/github/sac/Socket.java
@@ -483,6 +483,7 @@ public class Socket extends Emitter {
                     logger.info(name + value + "\n");
                 }
             }
+            reconnect();
         } catch (WebSocketException e) {
             listener.onConnectError(Socket.this, e);
             reconnect();
